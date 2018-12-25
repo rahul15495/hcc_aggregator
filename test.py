96823bb8c57e15a27aa4ee1b7a32bd7b4f98ef76
rahul chaurasia <rahul15495@bitbucket.org>
2018-12-25 22:43:37 +0530

    new v22 regvars working

--- test.py
+++ test.py
@@ -3,19 +3,20 @@ import handler
 
 def main():
     out = handler.get_scores(hicno='1906072835999',
-                                sex= 'M',
-                                dob= '1956-03-16',
+                                sex= 'F',
+                                dob= '1948-03-16',
                                 year_of_eligibility= '2018',
                                 RAF_type= 'CNA',
                                 orec= 1,
-                                medicaid= True,
-                                codes=['I209', 'I70203', 'E119', 'E119', 'E119', 'E119', 'E119']
+                                medicaid= False,
+                                codes=['E119', 'E119', 'E119', 'E119', 'E119', 'E119', 'E119']
                                 )
 
     print(out)
 
     # output
     # [{'cna_f65_69': 0.312}, {'cna_hcc19': 0.10400000000000001}, {'cna_originallydisabled_female': 0.244}]
+
 
 if __name__ == '__main__':
     main()
