f5a4277e1f521121881c4f3b9839078d9e133a76
rahul <rahul.8d@gmail.com>
2019-02-04 23:08:06 +0530

    api layer added

---
+++ api.py
@@ -0,0 +1,13 @@
+import handler
+
+def single_month_score_predcitor(**kwargs):
+
+    out = handler.get_scores(**kwargs)
+
+    return out
+
+
+def aggregate_score_predcitor(**kwargs):
+    print(kwargs)
+    
+    return 0
