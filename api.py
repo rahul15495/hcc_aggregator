e92e85ba561efe06568b0cd1fd3c647aab42e0b7
rahul kumar chaurasia <rahul.8d@gmail.com>
2019-02-26 21:27:47 +0530

    multiprocessing added to overcome pydatlog-engine issues

--- api.py
+++ api.py
@@ -1,8 +1,27 @@
-import handler as handler
+import importlib
+import multiprocessing as mp
+
+handler= None
+
+def handler_wrapper(q,*args):
+    
+    handler= importlib.import_module('handler')
+
+    args=args[0][0]
+    out = handler.get_scores(**args)
+    q.put(out)
 
 def single_month_score_predcitor(**kwargs):
 
-    out = handler.get_scores(**kwargs)
+    q= mp.Queue()
+
+    process=mp.Process(target=handler_wrapper, args=(q,[kwargs]) )
+
+    process.start()
+    
+    out =q.get()
+
+    process.join()
 
     return out
 
@@ -11,3 +30,5 @@ def aggregate_score_predcitor(**kwargs):
     print(kwargs)
     
     return 0
+
+#https://stackoverflow.com/questions/17503909/select-a-module-using-importlib-and-use-in-multiprocessing-functions
